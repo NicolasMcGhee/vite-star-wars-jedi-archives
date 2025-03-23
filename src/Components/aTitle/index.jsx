e5734ca6a4b2ab02257213e9e9ed5083a455ec44
EternalSuplex <32345511+Skeletonsarecool@users.noreply.github.com>
2025-03-23 10:21:50 -0500

    Title Commit

---
+++ src/Components/aTitle/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+export default function Title() {
+  return (
+    <div className="Title-Container">
+      <div className="Title-Title">
+        <h1> Star Wars Archives</h1>
+        <h3>Search for various information</h3>
+      </div>
+    </div>
+  );
+}
